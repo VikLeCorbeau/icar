e974348ae169bc5877a556a967016399a02255b4
Tim <80949502+TimCode7@users.noreply.github.com>
2021-05-03 19:17:07 +0200

    ajout $_SESSION['profil']

--- verificationConnexion.php
+++ verificationConnexion.php
@@ -5,6 +5,7 @@
 		while ($data = fgetcsv($fid, 1000, ';')) {
 			if ($data[0] == $_POST["id"] && $data[1] == $_POST["password"]) {
 				$_SESSION["identifiants"] = $data[0];
+				$_SESSION['profil'] = $data[2];
 				header('Location: accueil.php');
 				exit();
 			}
