3c06522690e9d0c0f4fe989da2166482b539a12c
Victor Baucor <baucor.victor@gmail.com>
2021-05-10 16:30:55 +0200

    Réalisation CSS connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -5,15 +5,57 @@
 <html>
 <head>
 	<title>Connexion</title>
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/generics.css" rel="stylesheet">
+    <link href="../css/connection.css" rel="stylesheet">
 </head>
 <body>
+
     <div class="main-container">
-        <div class="desktop-container">
-            <div class="nav-container">
-                <svg></svg>
+
+
+        <div class="nav-container" style="border: none;">
+            <div class="nav-container-1440">
+                <div class="nav-logo-container">
+                    <svg aria-hidden="true" class="logo">
+                        <use xlink:href="../assets/svg/logo.svg#logo"></use>
+                    </svg>
+                </div>
             </div>
         </div>
+
+        <div class="container-1440">
+
+
+            <div class="content-container content-center">
+
+                <form action="" method="get" class="form-connection">
+                    <h1 class="form-connection-title">Content de vous revoir !</h1>
+                    <div class="form-connection-input-container">
+                        <label for="name" class="form-connection-label">E-mail</label>
+                        <input type="text" name="name" id="name" class="form-connection-input" placeholder="Entrez votre adresse mail" required>
+                    </div>
+                    <div class="form-connection-input-container">
+                        <label for="password" class="form-connection-label">Mot de passe</label>
+                        <input type="password" name="password" id="password" class="form-connection-input" placeholder="Entrez votre mot de passe" required>
+                    </div>
+                    <div class="form-connection-input-container">
+                        <input type="submit" class="button button--dark" value="Connexion">
+                    </div>
+                </form>
+
+
+                <div class="illustration-connection-container">
+                    <img class="illustration-connection"
+                        src="../assets/images/illustrations/illustration_connection.png"
+                        alt="Grapefruit slice atop a pile of other slices">
+                </div>
+
+                
+            </div>
+
+        </div>
+
     </div>
+
 </body>
 </html>
