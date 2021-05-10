52e028ef5ceb3d13b67ef6acc4df86cce821596d
Vincent Arnaudin <varnaudin64@gmail.com>
2021-05-10 16:45:36 +0200

    Sex with me

--- pages/visiteur.php
+++ pages/visiteur.php
@@ -4,17 +4,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-       <meta charset="utf-8">
-        <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="../css/visiteur.css" media="screen" type="text/css" />
-    </head>
+    <meta charset="utf-8">
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="../css/visiteur.css" media="screen" type="text/css" />
+    <script type="text/javascript" src="../src/visiteur.js"></script>
+</head>
 <body>
+
+
+<!-- Récupère les info du joueur voulu et appelle la fonction rechercher -->
+<p>Nom : <input type="text" name="nom" id="nom" required></p>
+    <p>Prenom : <input type="text" name="prenom" id="prenom" required></p>
+    <p><input type="button" value="rechercher" onclick="rechercher()"></p>
+
     <h1>Carte verte de l'assurance</h1>
-    <p>Dates de validité de l’assurance :</p>
-    <p>Identification du véhicule : </p>
-    <p>Numéro de contrat d’assurance : </p>
-    <p>Nom de l’assurance : </p>
-    <p>Identifiez vous pour accedez a vos informations !</p>
+
+<!-- div ou seront écrit les statistiques -->
+    <div id="info"></div>
     <a href="connexion.php">Accedez à votre espace</a>
 
     <form action="verification.php" method="POST">
--- qrCode.php
+++ qrCode.php
@@ -1,12 +1,13 @@
 <?php 
 include('phpqrcode/qrlib.php');
-$lien = $_SERVER;
+$lien = "http://localhost/icar/pages/visiteur.php";
 QRcode::png($lien, 'image-qrcode.png');
  ?>
  <!DOCTYPE html>
  <html>
  <head>
  	<title>Qr code</title>
+	<script type="text/javascript" src="../src/visiteur.js"></script>
  </head>
  <body>
  	<p>Le qrCode de la page est : </p>
