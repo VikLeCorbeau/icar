bea41f552839ba92438409e55733134b67301a8b
Victor Baucor <baucor.victor@gmail.com>
2021-05-18 17:36:38 +0200

    SS

--- pages/visiteur.php
+++ pages/visiteur.php
@@ -59,22 +59,34 @@
                             <h2 class="box-title">Carte verte d'assurance</h2>
                         </div>
                         <div class="box-informations-container">
-                            <div class="box-informations">
-                                <h3 class="box-information box-information--primary">Nom de l'assurance :</h3>
-                                <h3 class="box-information box-information--secondary">Pacifica</h3>
-                            </div>
-                            <div class="box-informations">
-                                <h3 class="box-information box-information--primary">Numéro du contrat d'assurance :</h3>
-                                <h3 class="box-information box-information--secondary">6765432</h3>
-                            </div>
-                            <div class="box-informations">
-                                <h3 class="box-information box-information--primary">Numéro d'immatriculation :</h3>
-                                <h3 class="box-information box-information--secondary">ZZ-564-FR</h3>
-                            </div>
-                            <div class="box-informations">
-                                <h3 class="box-information box-information--primary">Date de validité :</h3>
-                                <h3 class="box-information box-information--secondary">23/03/2024</h3>
-                            </div>
+                        <?php 
+								$fa = fopen("../db/InfoAssure/".$nomAssure."/contrats.csv", 'r');
+								while ($data = fgetcsv($fa, 1000, ';')) {
+									if ($immatriculation == $data[7]) {
+										echo "<div class='box-informations'>";
+										echo "<h3 class='box-information box-information--primary'>Nom de l'assurance :</h3>";
+										echo "<h3 class='box-information box-information--secondary'>".$data[5]."</h3>";
+										echo "</div>";
+										echo "<div class='box-informations'>";
+										echo "<h3 class='box-information box-information--primary'>Numéro du contrat d'assurance :</h3>";
+										echo "<h3 class='box-information box-information--secondary'>".$data[6]."</h3>";
+										echo "</div>";
+										echo "<div class='box-informations'>";
+										echo "<h3 class='box-information box-information--primary'>Numéro d'immatriculation :</h3>";
+										echo "<h3 class='box-information box-information--secondary'>".$data[7]."</h3>";
+										echo "</div>";
+										echo "<div class='box-informations'>";
+										echo "<h3 class='box-information box-information--primary'>Date de validité :</h3>";
+										echo "<h3 class='box-information box-information--secondary'>".$data[8]."</h3>";
+										echo "</div>";
+										echo "<div class='box-informations'>";
+										echo "<h3 class='box-information box-information--primary'>Modèle du véhicule :</h3>";
+										echo "<h3 class='box-information box-information--secondary'>".$data[9]."</h3>";
+										echo "</div>";
+									}
+								}
+								fclose($fa);
+							?>
                         </div>
 
                     </div>
--- src/ajoutConstat2.php
+++ src/ajoutConstat2.php
@@ -4,51 +4,167 @@
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>Constat</title>
+ 		<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Constat</title>
+
+        <link href="../css/generics.css" rel="stylesheet">
+        <link href="../css/visiteur.css" rel="stylesheet">
+        <link href="../css/boxes.css" rel="stylesheet">
+        <link href="../css/form.css" rel="stylesheet">	
  </head>
  <body>
 
- 	<form id="car-selection" action="ajoutConstat2.php" method="POST">
+	<div class="main-container">
 
- 		<div class="custom-select">
+		<div class="nav-container">
+			<div class="nav-container-1440">
 
-			<select name="voitureA">
-				<option value="0">Select car:</option>
-				<option value="1">Audi</option>
-				<option value="2">BMW</option>
-				<option value="3">Citroen</option>
-				<option value="4">Ford</option>
-				<option value="5">Honda</option>
-				<option value="6">Jaguar</option>
-				<option value="7">Land Rover</option>
-				<option value="8">Mercedes</option>
-				<option value="9">Mini</option>
-				<option value="10">Nissan</option>
-				<option value="11">Toyota</option>
-				<option value="12">Volvo</option>
+				<div class="nav-logo-container">
+					<svg aria-hidden="true" class="logo">
+						<use xlink:href="../assets/svg/logo/logo.svg#logo"></use>
+					</svg>
+				</div>
 
-				<?php 
-					/*if ($fa = fopen('../db/InfoAssure/'.$_SESSION['identifiants'].'/contrats.csv', 'r')) {
-						while ($data = fgetcsv($fa, 1000, ';')) {
-							echo "<option value=".$data[9].">".$data[9]." ".$data[7]."</option>";
-						}
-						fclose($fa);
-					}*/
-				?>
+				<div class="nav-right-container">
 
-			</select>
+					<div class="nav-side-container">
 
+						<div class="nav-side-links-container">
+
+							<div class="nav-side-link-container">
+
+								<div class="nav-side-link">
+									<p class="nav-side-svg-text">Actions</p>
+									<img src="../assets/svg/icons/down_arrow.svg" class="nav-side-arrow">
+								</div>
+
+								<div class="nav-side-link-dropdown-container">
+									<ul class="nav-side-link-dropdown-ul">
+										<li class="nav-side-link-dropdown-li"><a href="">Déclarer un constat</a></li>
+										<li class="nav-side-link-dropdown-li"><a href="">Voir mes cartes vertes</a></li>
+										<li class="nav-side-link-dropdown-li"><a href="">Je vends mon véhicule</a></li>
+										<li class="nav-side-link-dropdown-li"><a href="">Contacter mon assurance</a></li>
+										<li class="nav-side-link-dropdown-li"><a href="">Mes déclarations de constat</a></li>
+									</ul> 
+								</div>
+
+							</div>
+
+							<div class="nav-side-link-container">
+
+								<div class="nav-side-link">
+									<svg aria-hidden="true" class="nav-side-svg">
+										<use xlink:href="../assets/svg/type/type.svg#type_insured"></use>
+									</svg>
+									<p class="nav-side-svg-text">Prénom Nom</p>
+
+								</div>
+
+								<div class="nav-side-link-dropdown-container">
+									<ul class="nav-side-link-dropdown-ul">
+										<li class="nav-side-link-dropdown-li"><a href="">Changer mes coordonnées</a></li>
+										<li class="nav-side-link-dropdown-li"><a href="">Déconnexion</a></li>
+									</ul> 
+								</div>
+
+							</div>
+
+						</div>
+						
+					</div>
+				</div>
+
+			</div>
+		</div>
+		
+		<div class="container-1440">
+			<div class="content-container content-column">
+
+				<div class="content-banner">
+						
+					<div class="content-titles-container">
+						<h1 class="content-title">Déclaration de constat</h1>
+						<h1 class="content-subtitle">2-Votre véhicule</h1>
+					</div>
+
+				</div>
+				
+
+				<form id="car-selection" action="ajoutConstat2.php" method="POST">
+
+					<div class="form-title-container">
+                        <h1 class="form-title">Sélection du véhicule concerné</h1>
+                    </div>
+
+                    <div class="grid-form">
+
+						<div class="input-container">
+
+							<div class="custom-select">
+
+								<select name="voitureA">
+									<option value="0">Select car:</option>
+									<option value="1">Audi</option>
+									<option value="2">BMW</option>
+									<option value="3">Citroen</option>
+									<option value="4">Ford</option>
+									<option value="5">Honda</option>
+									<option value="6">Jaguar</option>
+									<option value="7">Land Rover</option>
+									<option value="8">Mercedes</option>
+									<option value="9">Mini</option>
+									<option value="10">Nissan</option>
+									<option value="11">Toyota</option>
+									<option value="12">Volvo</option>
+
+									<?php 
+										/*if ($fa = fopen('../db/InfoAssure/'.$_SESSION['identifiants'].'/contrats.csv', 'r')) {
+											while ($data = fgetcsv($fa, 1000, ';')) {
+												echo "<option value=".$data[9].">".$data[9]." ".$data[7]."</option>";
+											}
+											fclose($fa);
+										}*/
+									?>
+
+								</select>
+
+							</div>
+
+						</div>
+
+						<div class="input-container">
+
+							<button form="car-selection" type="submit" class="button button--yellow">
+								<p class="button-text">Choisir</p>
+							</button>
+
+						</div>
+
+					</div>	
+
+				</form>
+
+			</div>
 		</div>
 
-		<div class="buttons-container">
-			<button form="car-selection" type="submit" class="button button--yellow">
-				<p class="button-text">Choisir</p>
-			</button>
-		</div>
+		<footer>
+			<div class="footer-center">
+				<div class="footer-box">
+					<h1 class="footer-title">contact</h1>
+					<h2 class="footer-text">05 67 34 56 78 - icar@contact.fr</h2>
+				</div>
+				<div class="footer-box">
+						<h1 class="footer-title">support</h1>
+					<h2 class="footer-text"><a href="#">Signaler une erreur aux adminstrateurs</a></h2>
+				</div>
+			</div>
+		</footer>
 
- 		<input type="submit" value="Choisir">
+	</div>
 
- 	</form>
+ 	
  	<?php 
  		if (isset($_POST['voitureA'])) {
  			$voiture = $_POST['voitureA'];
@@ -199,5 +315,7 @@
 		
  		<p><input type="submit" value="continuer"></p>
  	</form>
+
+	 
  </body>
  </html>
